d4cfe9f3f885760de022941ffb2c2a61ad782a82
Alan Verbner <alverbner@gmail.com>
2025-07-16 16:30:45 -0300

    fix(build): added base path

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
-{
+module.exports = {
   "title": "CODE: OVERRIDE - MiniTel-Lite Emergency Protocol",
   "description": "MiniTel-Lite Emergency Protocol",
+  "baseUrl": process.env.NODE_ENV=== "production" ? "/challenge-landing/" : "/",
   "form_endpoint": "https://forms.google.com/XXX",
   "server_host": "demo.minitel-lite.xyz",
   "server_port": 7321,

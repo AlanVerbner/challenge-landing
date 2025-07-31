8140b9167145828c2dd5ffe4f237a461c76db2db
Alan Verbner <alverbner@gmail.com>
2025-07-31 11:55:04 -0300

    feat: base url updated for new prod url http://minitel.ai-quests.dev/

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
   title: "CODE: OVERRIDE - MiniTel-Lite Emergency Protocol",
   description: "MiniTel-Lite Emergency Protocol",
-  baseUrl: process.env.NODE_ENV === "production" ? "/challenge-landing/" : "/",
+  baseUrl: process.env.NODE_ENV === "production" ? "/" : "/",
   form_endpoint:
     "https://docs.google.com/forms/d/1X6GQ7gHcakXpTlpRh9x-NufSZNnaaRZbGvAA_SZz7RU/",
   server_host: "IP_ADDRESS_AND_PORT_PROVIDED_OVER_SLACK",
-  url: "https://yourusername.github.io/code-override",
+  url: "http://minitel.ai-quests.dev",
   author: "NORAD",
   winner_amount: 3,
   budget: 10,
